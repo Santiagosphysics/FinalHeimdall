2f1f1b8fdd940b7f7e178a12009ca57c9ec2443b
santiago <mijimenez@unbosque.edu.co>
2025-05-08 13:51:38 -0500

    1.1.6

--- indicators.py
+++ indicators.py
@@ -1,11 +1,11 @@
 from utils import models, get_data_crypto, meassures
 
-# data = get_data_crypto().download_data_cloud(start_time='2025-04-25', end_time='2025-04-30 13:00:00', crypto="BNBUSDT", time='S')
-# prophet_model = models.prophet_model(data=data, time='S')
 
 import pandas as pd 
 import datetime
 import matplotlib.pyplot as plt 
+import matplotlib.dates as mdates
+import numpy as np  
 
 
 class indicators:
@@ -26,6 +26,8 @@ class indicators:
 
         plt.plot(first_pred['ds'], first_pred['y'], label='Fine prediction')
         plt.plot(second_pred['ds'], second_pred['y'], label = 'Large prediction', c='r')
+
+
         plt.title('Comparative  between two trains')
         plt.ylabel('Price')
         plt.xlabel('Date')
@@ -47,29 +49,47 @@ class indicators:
         first_data = get_data_crypto().download_data(start_time=midle_time, end_time=end_time, crypto=crypto, time=time)
         second_data = get_data_crypto().download_data(start_time=start_time, end_time=end_time, crypto=crypto, time=time)
         third_data = get_data_crypto().download_data(start_time=end_time, end_time= future_time, crypto=crypto, time=time)
-        third_data = third_data.rename(columns={'close_time':'ds','close':'y'})
+
+        third_data = third_data.rename(columns={'close_time':'ds','close':'Real Price'})
 
         df1, first_pred = models().XGBoost_model(data=first_data, time=time)
         df2, second_pred = models().XGBoost_model(data=second_data, time=time)
 
         first_pred = first_pred.rename(columns={'y':'Fine Pred'})
+        second_pred = second_pred.rename(columns= {'y':'Large Pred'})
 
-        first_pred = first_pred[['ds', 'y']]
-        second_pred = second_pred[['ds', 'y']]
+
+        first_pred = first_pred[['ds', 'Fine Pred']]
+        second_pred = second_pred[['ds', 'Large Pred']] 
 
         data = pd.merge(left=third_data, right = second_pred, on='ds',how='left')
+        data = pd.merge(left=data, right=first_pred, on='ds', how='left')
+
+
+        data['Diff Fine'] = np.absolute(data['Real Price'] - data['Fine Pred'])
+        data['Diff Large'] = np.absolute(data['Real Price'] - data['Large Pred'])
+
+        data = data.dropna(subset=['Diff Fine', 'Diff Large'], ignore_index=True)
+
+        print(np.mean(data['Diff Fine']), ' ',np.mean(data['Diff Large']) )
+
+
+        difference_fine = round(np.trapezoid(data['Diff Fine'], x=mdates.date2num(data['ds'])), 2)
+        difference_large = round(np.trapezoid(data['Diff Large'], x=mdates.date2num(data['ds'])), 2)
+        
+        min_price = round(third_data['Real Price'].min(), 2)*2/100
 
         plt.figure(figsize=(10,6))
-        plt.plot(first_pred['ds'], first_pred['y'], label='Fine prediction', c='black')
-        plt.plot(second_pred['ds'], second_pred['y'], label = 'Large prediction', c='r')
-        plt.plot(third_data['ds'], third_data['y'], label = 'Real Price', c='green')
+        plt.plot(data['ds'], data['Fine Pred'], label='Fine prediction', c='black')
+        plt.plot(data['ds'], data['Large Pred'], label = 'Large prediction', c='r')
+        plt.plot(data['ds'], data['Real Price'], label = 'Real Price', c='green')
 
-        plt.fill_between(third_data['ds'], second_pred['y'], third_data['y'], label='Mismatch Large P')
-        plt.fill_between(third_data['ds'], first_pred['y'], third_data['y'], label = 'Mismatch Fine P')
+        plt.fill_between(data['ds'], data['Fine Pred'], data['Real Price'], label='Diff Real VS Fine P', alpha=0.2)
+        plt.fill_between(data['ds'], data['Large Pred'], data['Real Price'], label='Diff Real VS Large P', alpha = 0.2)
 
-        plt.title(f"Prediction since {third_data['ds'].min()} until {third_data['ds'].max()}")
+        plt.title(f"Diff Fine: {difference_fine}, Diff Large: {difference_large}\n Min Profit {min_price}" )
         plt.ylabel('Price')
-        plt.xlabel('Date')
+        plt.xlabel(f"Prediction since {third_data['ds'].min()} until {third_data['ds'].max()}")
         plt.legend()
         plt.grid(True)
         plt.show()
@@ -80,10 +100,11 @@ class indicators:
 
 end_time = '2025-03-15 00:00:00'
 
-days_fine_pred = 1
-days_pred = 5
+days_fine_pred = 5
+days_pred = 10
 crypto='ETHUSDT'
-time='min'
+time='S'
 
 
 test_1 = indicators().development_model(end_time = end_time, days_fine_pred=days_fine_pred, days_pred = days_pred, crypto = crypto, time = time)
+#test_1 = indicators().predictions(days_fine_pred=days_fine_pred, days_pred=days_pred, crypto=crypto, time=time)
